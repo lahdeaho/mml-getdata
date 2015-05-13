3ed731b4ec87aa83f57c2da54d4882b923f982fb
Mikko Lähdeaho <mikko.lahdeaho@nbl.fi>
2015-05-13 23:03:40 +0300

    commit

---
+++ download_data.R
@@ -0,0 +1,4 @@
+##tuotteet/maastotietokanta/tiesto_osoitteilla/etrs89/gml/L41.zip
+##{"timestamp":"2015-05-13T14:34:14.934Z","customer":{"licenseAccepted":true,"firstname":"Mikko","lastname":"Lähdeaho","organization":null,"email":"mikko.lahdeaho@invenco.fi","language":"fi"},"files":["/tuotteet/maastotietokanta/kaikki/etrs89/gml/L4/L41/L4133L_mtk.zip","/tuotteet/maastotietokanta/kaikki/etrs89/gml/L4/L41/L4133_mtk.zip"]}
+
+##https://tiedostopalvelu.maanmittauslaitos.fi/tp/tilaus
